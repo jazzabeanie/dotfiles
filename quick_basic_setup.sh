452d2d16b1e3b5edf25b9d2fe97e7988fef3d1ba
Jared Johnston <jazzab@gmail.com>
2022-02-09 14:52:07 +1000

    Add quick setup
    Also, tmux shortcut hints

---
+++ quick_basic_setup.sh
@@ -0,0 +1,4 @@
+mv  ~/.bashrc ~/.bashrc_backup
+curl -o ~/.vimrc https://raw.githubusercontent.com/jazzabeanie/dotfiles/master/home/.vimrc
+curl -o ~/.bashrc https://raw.githubusercontent.com/jazzabeanie/dotfiles/master/home/.bashrc
+curl -o ~/.aliases https://raw.githubusercontent.com/jazzabeanie/dotfiles/master/home/.aliases
