ca3b4fbe6470e5bcddacceffa6565e9a678afdf8
Jared Johnston <jazzab@gmail.com>
2024-03-08 09:04:52 +1000

    Add sshfs

--- install_packages.sh
+++ install_packages.sh
@@ -36,6 +36,7 @@ sudo apt install --yes tree
 sudo apt install --yes gnome-tweaks
 sudo apt install --yes keychain
 sudo apt install --yes bc # precision calculator
+sudo apt install --yes sshfs # let's me mount a remote folder locally
 
 # visidata: apt install old version. Use pip3 as per install instuctions https://www.visidata.org/install/
 # https://github.com/ajeetdsouza/zoxide # z (smarter version of cd)
