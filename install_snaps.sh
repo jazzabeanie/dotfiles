c4bc4fa1e0bac41476c43d46f945b6991db4c2c0
Jared Johnston <jazzab@gmail.com>
2026-02-23 17:32:45 +1000

    remove snaps installed elsewhere

--- install_snaps.sh
+++ install_snaps.sh
@@ -1,11 +1,11 @@
 # TODO: set up a script to install with snapd:
 
 # sudo snap install cheat  # cheat - https://github.com/cheat/cheat # use cheat.sh instead
-sudo snap install fx # https://github.com/antonmedv/fx
+# sudo snap install fx # https://github.com/antonmedv/fx
 # sudo snap install slack
 # sudo snap install teams
-sudo snap install diff-so-fancy
-sudo snap install lsd
+# sudo snap install diff-so-fancy
+# sudo snap install lsd
 # sudo snap install --classic nvim
 # sudo snap install direnv  # Use snap or use apt?
 # sudo snap install pinta
